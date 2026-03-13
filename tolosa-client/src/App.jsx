ca54b5d9c051efec7a8e021a0e0ee547a800a04c
unknown <joshuamaquilan31@gmail.com>
2026-03-13 16:02:09 +0800

    Tolosa-client

--- tolosa-client/src/App.jsx
+++ tolosa-client/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './App.css';
-
 function App(){
-  return (
-    <div className="App">
-      <header className="Äpp-header">
-        <h1>Welcome to My React App!</h1>
-        <p>
-          Name: Tolosa, Alden Jr. Z. <br/>
-          Section: INF 235 <br/>
-          Other Personal Info:
-          <a href='https://github.com/kyrieee11/Tolosa-webprog'></a>Githublink
-        </p>
-      </header>
-    </div>
-  )
+  return (
+    <div className="App">
+      <header className="Äpp-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Tolosa, Alden Jr. Z. <br/>
+          Section: INF 235 <br/>
+          Githublink: https://github.com/kyrieee11/Tolosa-webprog<br/>
+        </p>
+      </header>
+    </div>
+  )
 }
+
+export default App;
